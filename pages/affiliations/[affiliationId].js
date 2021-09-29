1d4f04cca407985dec57402b21d57237846443c7
colbyfayock <fayock@gmail.com>
2021-09-28 22:04:54 -0400

    Step 4:  fetching characters by affiliation at request time with getServerSideProps

---
+++ pages/affiliations/[affiliationId].js
@@ -0,0 +1,29 @@
+export default function Affiliation({ characters, affiliationId }) {
+  return (
+    <div>
+      <h1>Characters Matching Affiliation "{affiliationId.replace(/\+/g, ' ')}"</h1>
+      <ul style={{ display: 'grid', 'grid-template-columns': '1fr 1fr 1fr', 'grid-gap': '4em' }}>
+        {characters.map(character => {
+          return (
+            <li key={character._id}>
+              <p>{ character.name }</p>
+              <p>{ character.affiliation }</p>
+              <img src={character.photoUrl} width="100%" alt="" />
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export async function getServerSideProps({ params }) {
+  const affiliationId = params.affiliationId.replace(/\-/g, '+')
+  const characters = await fetch(`https://last-airbender-api.herokuapp.com/api/v1/characters?affiliation=${affiliationId}`).then(res => res.json());
+  return {
+    props: {
+      affiliationId,
+      characters
+    }
+  }
+}
